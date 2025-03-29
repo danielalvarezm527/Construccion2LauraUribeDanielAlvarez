568f1949d54c03da77f07fd1339c6ec2b2350183
danielQuatio <daniel.alvarez@quatio.co>
2025-03-29 14:16:02 -0500

    ADD Bill port, CHG Bill model, SellerService, veterinarian service
    se agrego el billport
    se corrigieron unas validadciones en el servicio del veterinario, dentro de ese mismo servicio se agregaron los metodos para crear y cancelar orden, pero quedan unas preguntas que no se todavia como se van a hacer
    se agregaron los metodos al seller service para vender medicina u otros productos y generar una factura
    por ultimo se creo el constructor para el modelo bill

--- src/main/java/app/domain/models/Bill.java
+++ src/main/java/app/domain/models/Bill.java
@@ -1,14 +1,30 @@
 package app.domain.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.sql.Timestamp;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class Bill {
 	private long billId;
-	private Pet petId;
-	private Order orderId;
-	private String productName;
-	private double value;
-	private int amount;
-	private Timestamp date; 
-	
+    private Pet petId;
+    private Order orderId;
+    private String productName;
+    private double value;
+    private int amount;
+    private Timestamp date; 
+    
+    public Bill(long billId, Pet petId, Order orderId, String productName, double value, int amount, Timestamp date) {
+        this.billId = billId;
+        this.petId = petId;
+        this.orderId = orderId;
+        this.productName = productName;
+        this.value = value;
+        this.amount = amount;
+        this.date = date;
+    }
 }
